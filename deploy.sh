f9629404c97630e3c825bdd74ee6d2ec5f9e8837
houqiuhao <295142489@qq.com>
2022-02-28 23:55:40 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -13,7 +13,7 @@ cd docs/.vuepress/dist
 # echo 'www.example.com' > CNAME
 
 # git init
-git add ./docs
+git add ./
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
