66a39e40f46f459e094bed94522e0168e0db70fe
houqiuhao <295142489@qq.com>
2022-03-20 00:09:19 +0800

    增加valine 评论功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,11 @@
 const themeConfig = require('./config/themeConfig')
+const plugins = require('./config/plugins')
 module.exports = {
     title: "Hammer's blog",
     description: '愿知晓并笃行',
     theme: require.resolve('../../vdoing'),//'vdoing',
     themeConfig,
     base: "/vuepressBolg/",
+    plugins,
     // dest: "dist"
 }
--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,14 +1,13 @@
 module.exports = [
-
-    [
-        '@vuepress/last-updated', // "上次更新"时间格式
-        {
-          transformer: (timestamp, lang) => {
-            const dayjs = require('dayjs') // https://day.js.org/
-            return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
-          },
-        },
-    ],
+    // [
+    //     '@vuepress/last-updated', // "上次更新"时间格式
+    //     {
+    //       transformer: (timestamp, lang) => {
+    //         const dayjs = require('dayjs') // https://day.js.org/
+    //         return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+    //       },
+    //     },
+    // ],
     [
         'vuepress-plugin-zooming', // 放大图片
         {
@@ -18,5 +17,17 @@ module.exports = [
           },
         },
     ],
+    [
+        'vuepress-plugin-comment',
+        {
+          choosen: 'valine', 
+          // options选项中的所有参数，会传给Valine的配置
+          options: {
+            el: '#valine-vuepress-comment',
+            appId: 'N9EVfa11nwxftvNc1oi2nJRk-gzGzoHsz',
+            appKey: '9zLL7Xo2BU8tlnjiMccJLGPk'
+          }
+        }
+    ]
 
 ]
